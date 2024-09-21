106ef640351362d58c06168e80caf1bcf6171204
Ryan Merlini <rdmerlini@gmail.com>
2024-09-21 18:53:42 -0400

    added table of contents

---
+++ ReadMe.md
@@ -0,0 +1,64 @@
+# ReadMeGenerator
+
+This CLI app will let you create your own ReadMe file from CLI inputs
+
+## TOC
+
+- [ReadMeGenerator](#readmegenerator)
+	- [TOC](#toc)
+	- [Installation](#installation)
+	- [Usage](#usage)
+	- [video notes:](#video-notes)
+	- [Contributions](#contributions)
+	- [License](#license)
+	- [Contact](#contact)
+	- [Acknowledgments](#acknowledgments)
+  
+
+## Installation
+
+git clone git@github.com:Robo-Ryan/ReadMeGenerator.git
+
+<!-- node installation: Navigate to your directory in terminal then input the following-->
+npm init -y  
+npm i inquirer  
+npm i colors  
+npm install  
+<!-- make sure “type:” “module” are added to the package.json file -->
+
+
+## Usage
+
+- Watch this video on how to use the ReadMe Generator
+- To invoke the app from your CLI use the command:
+node index
+
+video notes:
+- 
+- The walkthrough video must demonstrate how a user would invoke the application from the command line.
+- The walkthrough video must demonstrate how a user would enter responses to all of the prompts in the application.
+- The walkthrough video must demonstrate a generated README that matches the user input and has a functioning table of contents.How to use the project, including code blocks and screenshots as necessary.
+npm start
+
+## Contributions
+
+Contributing  
+	1.	Fork the project  
+	2.	Create your feature branch (git checkout -b feature/featurename)  
+	3.	Commit your changes (git commit -m 'Added a feature')  
+	4.	Push to the branch (git push origin feature/featurename)  
+	5.	Open a pull request  
+
+
+## License
+
+This is an MIT License - see the LICENSE file for details.
+
+## Contact
+
+Your Name – rdmerlini@gmail.com 
+Project Link: https://github.com/Robo-Ryan/ReadMeGenerator
+
+## Acknowledgments
+
+- Great lessons on node.js this last week, thanks Ben and Allan. 
--- index.js
+++ index.js
@@ -81,38 +81,42 @@ inquirer
     .prompt(questions)
     .then((response) => {
       let ReadMe = 
-      `
-      # ${response.title}
-     ${response.description}
-      
-      ## Installation
-      git clone ${response.gitHub}
-      cd ${response.title}
-      npm install
-      
-      ## Usage
-      [brief description of usage]
-      npm start
-      
-      ## Contributions
-      Contributing
-          1.	Fork the project
-          2.	Create your feature branch (git checkout -b feature/featurename)
-          3.	Commit your changes (git commit -m 'Added a feature')
-          4.	Push to the branch (git push origin feature/featurename)
-          5.	Open a pull request
-      
-      
-      ## License
-      This is an ${response.license} - see the LICENSE file for details.
-      
-      ## Contact
-      Your name: ${response.name}
-      Your email: ${response.email}
-      Project Link: ${response.github}
-      
-      ## Acknowledgments
-          •	Lorem ipsum`
+`
+# ${response.title}
+${response.description}
+
+- [${response.title}](#${response.title})
+	- [Installation](#installation)
+	- [Usage](#usage)
+	- [Contributions](#contributions)
+	- [License](#license)
+	- [Contact](#contact)
+	- [Acknowledgments](#acknowledgments)
+
+## Installation
+git clone ${response.github}
+cd ${response.title}
+
+## Usage
+
+
+## Contributions
+Contributing
+    1.	Fork the project
+    2.	Create your feature branch (git checkout -b feature/featurename)
+    3.	Commit your changes (git commit -m 'Added a feature')
+    4.	Push to the branch (git push origin feature/featurename)
+    5.	Open a pull request
+
+
+## License
+This is an ${response.license} - see the LICENSE file for details.
+
+## Contact
+Your name: ${response.name}
+Your email: ${response.email}
+Project Link: ${response.github}
+`
   fs.writeFile(`readmeexample.md`, ReadMe, (err)=>
   err ? console.error(err) : console.log(`Success!`))
 })
